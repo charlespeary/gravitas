77386ddcf19e7d6351a9b88bf5e0656c09b2db89
Karol Gruszka <karolgruszka9@gmail.com>
2021-07-18 15:39:10 +0200

    feat(vm): Opcode::Asg

--- crates/vm/src/memory.rs
+++ crates/vm/src/memory.rs
@@ -12,7 +12,13 @@ impl VM {
     }
 
     pub(crate) fn op_asg(&mut self) -> OperationResult {
-        unimplemented!()
+        let to_assign = self.pop_operand()?;
+        let address = self.pop_number()?;
+        let stack_start = self.current_frame().stack_start;
+
+        self.operands[stack_start + address as usize] = to_assign;
+
+        Ok(())
     }
 
     pub(crate) fn op_get(&mut self) -> OperationResult {
@@ -88,6 +94,45 @@ mod test {
 
         assert!(leftover_value
             .eq(RuntimeValue::Bool(true), &mut vm)
+            .unwrap());
+
+        Ok(())
+    }
+
+    #[test]
+    fn op_asg() -> OperationResult {
+        let mut vm = new_vm(Chunk::new(
+            vec![
+                Opcode::Constant(0),
+                Opcode::Constant(1),
+                Opcode::Constant(2),
+                Opcode::Asg,
+            ],
+            vec![
+                Constant::Number(127.0),
+                Constant::Number(0.0),
+                Constant::Number(7.0),
+            ],
+        ));
+
+        // push the constants onto the stack
+        vm.tick()?;
+        vm.tick()?;
+        vm.tick()?;
+
+        // the first operand on the stack is the initial value of the variable
+        let first_operand = vm.operands[0].clone();
+        assert!(first_operand
+            .eq(RuntimeValue::Number(127.0), &mut vm)
+            .unwrap());
+
+        // execute Opcode::Asg
+        vm.tick()?;
+
+        // but after the execution the first operand on the stack will change to the assigned value
+        let assigned_value = vm.operands[0].clone();
+        assert!(assigned_value
+            .eq(RuntimeValue::Number(7.0), &mut vm)
             .unwrap());
 
         Ok(())
